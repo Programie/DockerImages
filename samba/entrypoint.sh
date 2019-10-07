5eaa48315bbaa1f2db3b096985acfa8447baceb6
Michael Wieland <git@selfcoders.com>
2019-10-08 00:37:26 +0200

    Parse users to create from "; create-user:<name>:<id>" comments

--- samba/entrypoint.sh
+++ samba/entrypoint.sh
@@ -3,17 +3,19 @@
 users=$(while read -r line; do
     line=$(echo ${line})
 
-    if [[ ${line} =~ ^valid\ users ]]; then
-        echo ${line} | cut -d "=" -f 2 | tr ' ' '\n'
+    if [[ ${line} =~ ^\;\ create-user: ]]; then
+        echo ${line} | cut -d ":" -f 2,3 | tr ' ' '\n'
     fi
 done < /etc/samba/smb.conf | sort | uniq)
 
-for user in ${users}; do
+for user_line in ${users}; do
+    user=$(echo ${user_line} | cut -d ":" -f 1)
+    user_id=$(echo ${user_line} | cut -d ":" -f 2)
     if grep -q "^${user}:" /etc/passwd; then
         continue
     fi
 
-    useradd -d /nonexistent -g users -s /usr/sbin/nologin ${user}
+    useradd -d /nonexistent -g users -s /usr/sbin/nologin -u ${user_id} ${user}
 done
 
 exec "$@"
