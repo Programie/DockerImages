35ddd6378bb3137487ca5deeba8a91e55f0fc725
Michael Wieland <git@selfcoders.com>
2020-11-02 23:12:10 +0100

    Fix output

--- ocrmypdf-inotify/ocrmypdf-inotify-helper.sh
+++ ocrmypdf-inotify/ocrmypdf-inotify-helper.sh
@@ -4,8 +4,6 @@ base_dir="$1"
 input_filename="$2"
 output_filename="${input_filename%.pdf}_ocr.pdf"
 
-echo "Base dir: $1, Input filename: $2"
-
 # Input file is not a PDF file
 if [[ ${input_filename} != *.pdf ]]; then
     exit
@@ -16,10 +14,12 @@ if [[ ${input_filename} == *_ocr.pdf ]]; then
     exit
 fi
 
-echo "${input_filename} -> ${output_filename}"
+echo "Processing ${input_filename}"
 
 ocrmypdf -l ${OCRMYPDF_LANG:-eng} ${base_dir}/${input_filename} ${base_dir}/${output_filename}
 
+echo "Saved to ${output_filename}"
+
 if [[ -n ${OCRMYPDF_CHOWN} ]]; then
-    chown ${OCRMYPDF_CHOWN} ${base_dir}/${output_filename}
+    chown -c ${OCRMYPDF_CHOWN} ${base_dir}/${output_filename}
 fi
