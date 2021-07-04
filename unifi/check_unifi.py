16ec5fbdbbe993926b9940e56b36810798ddfb18
Michael Wieland <git@selfcoders.com>
2021-07-04 15:30:34 +0200

    Catch exception if something went wrong with a single AP

--- unifi/check_unifi.py
+++ unifi/check_unifi.py
@@ -2,6 +2,7 @@
 
 import os
 import re
+import sys
 
 from pyunifi.controller import Controller
 
@@ -32,38 +33,41 @@ for client in controller.get_clients():
     ap_clients[ap_mac].append(client)
 
 for ap in controller.get_aps():
-    ap_mac = ap["mac"]
+    try:
+        ap_mac = ap["mac"]
 
-    if ap_mac in ap_clients:
-        clients = ap_clients[ap_mac]
-    else:
-        clients = []
+        if ap_mac in ap_clients:
+            clients = ap_clients[ap_mac]
+        else:
+            clients = []
 
-    if ap["state"] == 1:
-        status = 0
-        status_string = "Online"
-    else:
-        status = 2
-        status_string = "Offline (!!)"
+        if ap["state"] == 1:
+            status = 0
+            status_string = "Online"
+        else:
+            status = 2
+            status_string = "Offline (!!)"
 
-    client_count = len(clients)
+        client_count = len(clients)
 
-    performance_data = {
-        "rx_bytes": ap["rx_bytes"],
-        "tx_bytes": ap["tx_bytes"],
-        "uptime": ap["uptime"],
-        "clients": client_count
-    }
+        performance_data = {
+            "rx_bytes": ap["rx_bytes"],
+            "tx_bytes": ap["tx_bytes"],
+            "uptime": ap["uptime"],
+            "clients": client_count
+        }
 
-    performance_data_array = []
+        performance_data_array = []
 
-    for key, value in performance_data.items():
-        performance_data_array.append("{}={}".format(key, value))
+        for key, value in performance_data.items():
+            performance_data_array.append("{}={}".format(key, value))
 
-    status_text = [
-        status_string,
-        "{} clients".format(client_count),
-        "Traffic: {} up / {} down".format(sizeof_fmt(ap["rx_bytes"]), sizeof_fmt(ap["tx_bytes"]))
-    ]
+        status_text = [
+            status_string,
+            "{} clients".format(client_count),
+            "Traffic: {} up / {} down".format(sizeof_fmt(ap["rx_bytes"]), sizeof_fmt(ap["tx_bytes"]))
+        ]
 
-    print("{} UniFi_Controller_{} {} {}".format(status, re.sub(r"\W+", "_", ap["name"]), "|".join(performance_data_array), ", ".join(status_text)))
+        print("{} UniFi_Controller_{} {} {}".format(status, re.sub(r"\W+", "_", ap["name"]), "|".join(performance_data_array), ", ".join(status_text)))
+    except BaseException as exception:
+        print(exception, file=sys.stderr)
