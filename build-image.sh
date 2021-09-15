6641154a243044f547bdc3764471fc05daf1d33a
Michael Wieland <git@selfcoders.com>
2021-09-15 23:16:28 +0200

    Use buildx to allow building multiarch images

--- build-image.sh
+++ build-image.sh
@@ -2,12 +2,14 @@
 
 image="$1"
 tag="$2"
+multiarch="$3"
 
 if [[ -z ${image} ]]; then
-    echo "Usage: $0 <image name> [<tag>]"
+    echo "Usage: $0 <image name> [<tag>] [multiarch]"
     exit 1
 fi
 
+shift
 shift
 shift
 
@@ -15,8 +17,14 @@ if [[ -z ${tag} ]]; then
     tag="latest"
 fi
 
+if [[ -z ${multiarch} ]]; then
+    platform="linux/amd64"
+else
+    platform="linux/amd64,linux/arm/v7,linux/arm64/v8"
+fi
+
 script_dir=$(dirname $(realpath $0))
 dir="${script_dir}/${image}"
 full_tag="registry.gitlab.com/programie/dockerimages/${image}:${tag}"
 
-docker build --pull --no-cache "$@" -t ${full_tag} ${dir} && docker push ${full_tag}
+docker buildx build --pull --push --no-cache "$@" --tag "${full_tag}" --platform "${platform}" "${dir}"
