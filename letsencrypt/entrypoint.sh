e812adfc6e90cb788e532bed530041bfaef07c2d
Michael Wieland <git@selfcoders.com>
2021-09-17 11:46:08 +0200

    Use Python for create-certs to make config parsing easier

--- letsencrypt/entrypoint.sh
+++ letsencrypt/entrypoint.sh
@@ -2,7 +2,7 @@
 
 case "$1" in
     docker-gen)
-        docker-gen -watch -notify "/app/create-certs.sh" /app/create-certs.tmpl /app/create-certs.conf
+        docker-gen -watch -notify "/app/create-certs.py" /app/create-certs.tmpl /app/create-certs.conf
     ;;
 
     *)
