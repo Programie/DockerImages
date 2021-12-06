bf93152ff855d413e63de97ccd9870e98a7a84de
Michael Wieland <git@selfcoders.com>
2021-12-06 18:53:17 +0100

    Add optional check for uplink speed

--- unifi/check_unifi.py
+++ unifi/check_unifi.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
-
+import json
 import os
 import re
 import sys
@@ -9,6 +9,7 @@ from pyunifi.controller import Controller
 username = os.getenv("UNIFI_API_USERNAME")
 password = os.getenv("UNIFI_API_PASSWORD")
 site_id = os.getenv("UNIFI_API_SITEID", "default")
+expected_uplink_speed = os.getenv("UNIFI_UPLINK_SPEED")
 ssl_verify = os.path.join(os.getenv("CERTDIR"), os.getenv("CERTNAME"))
 
 
@@ -20,7 +21,7 @@ def sizeof_fmt(num, suffix="B"):
     return "%.1f%s%s" % (num, "Y", suffix)
 
 
-controller = Controller(host="localhost", port=8443, username=username, password=password, site_id=site_id, ssl_verify=ssl_verify)
+controller = Controller(host="unifi.selfcoders.org", port=443, username=username, password=password, site_id=site_id, ssl_verify=ssl_verify)
 
 ap_clients = {}
 
@@ -41,12 +42,24 @@ for ap in controller.get_aps():
         else:
             clients = []
 
+        status = 0
+        status_text = []
+
         if ap["state"] == 1:
-            status = 0
-            status_string = "Online"
+            status = max(status, 0)
+            status_text.append("Online")
         else:
-            status = 2
-            status_string = "Offline (!!)"
+            status = max(status, 2)
+            status_text.append("Offline (!!)")
+
+        uplink_speed = int(ap.get("uplink", {}).get("speed", 0))
+
+        if expected_uplink_speed is not None:
+            expected_uplink_speed = int(expected_uplink_speed)
+
+            if uplink_speed != expected_uplink_speed:
+                status = max(status, 2)
+                status_text.append("Uplink: {} MBit/s but expected {} MBit/s (!!)".format(ap["uplink"]["speed"], expected_uplink_speed))
 
         client_count = len(clients)
 
@@ -54,6 +67,7 @@ for ap in controller.get_aps():
             "rx_bytes": ap["rx_bytes"],
             "tx_bytes": ap["tx_bytes"],
             "uptime": ap["uptime"],
+            "uplink_speed": uplink_speed,
             "clients": client_count
         }
 
@@ -62,11 +76,8 @@ for ap in controller.get_aps():
         for key, value in performance_data.items():
             performance_data_array.append("{}={}".format(key, value))
 
-        status_text = [
-            status_string,
-            "{} clients".format(client_count),
-            "Traffic: {} up / {} down".format(sizeof_fmt(ap["rx_bytes"]), sizeof_fmt(ap["tx_bytes"]))
-        ]
+        status_text.append("{} clients".format(client_count))
+        status_text.append("Traffic: {} up / {} down".format(sizeof_fmt(ap["rx_bytes"]), sizeof_fmt(ap["tx_bytes"])))
 
         print("{} UniFi_Controller_{} {} {}".format(status, re.sub(r"\W+", "_", ap["name"]), "|".join(performance_data_array), ", ".join(status_text)))
     except BaseException as exception:
