065699f734420121a445bea2a2be17f4e78c8218
Michael Wieland <git@selfcoders.com>
2022-10-07 16:56:17 +0200

    Add image for ffmpeg

---
+++ ffmpeg/record-stream.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+url="$1"
+output_dir="$2"
+
+if [[ -z ${url} ]] || [[ -z ${output_dir} ]]; then
+    echo "Usage: $0 <url> <output dir>"
+    exit 1
+fi
+
+while true; do
+    mkdir -p "${output_dir}/$(date +%Y/%m/%d)"
+
+    start_time=$(date +%s)
+
+    ffmpeg \
+        -i "${url}" \
+        -f segment \
+        -reset_timestamps 1 \
+        -strftime 1 \
+        -segment_time 1800 \
+        -segment_format mp4 \
+        -segment_format_options "movflags=frag_keyframe+empty_moov+default_base_moof" \
+        -c copy \
+        -map 0 \
+        "${output_dir}/%Y/%m/%d/%Y-%m-%d_%H-%M-%S.mp4"
+
+    end_time=$(date +%s)
+    duration=$((end_time - start_time))
+
+    if [[ ${duration} -lt 10 ]]; then
+        sleep 10
+    fi
+done
