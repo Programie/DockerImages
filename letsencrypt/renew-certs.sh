c8ac8d8a2431eb4cea1a7c3bf8af518bb6628ae6
Michael Wieland <git@selfcoders.com>
2022-10-30 11:47:30 +0100

    Remove letsencrypt image

--- letsencrypt/renew-certs.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-certbot renew --rsa-key-size 4096 --deploy-hook "/app/restart-container.sh ${LETSENCRYPT_CONTAINER_RESTART_RENEW}"
--- letsencrypt/restart-container.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-container="$1"
-
-if [[ -z ${container} ]]; then
-    exit
-fi
-
-curl --unix-socket /tmp/docker.sock -X POST http://localhost/containers/${container}/restart
