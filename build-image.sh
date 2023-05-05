57a581eb723ad3d079692d920ddac0953d291c80
Michael Wieland <git@selfcoders.com>
2023-05-05 16:51:26 +0200

    Remove multiarch as it is not used anymore

--- build-image.sh
+++ build-image.sh
@@ -2,7 +2,6 @@
 
 image="$1"
 tag="$2"
-multiarch="$3"
 
 if [[ -z ${image} ]]; then
     echo "Usage: $0 <image name> [<tag>] [multiarch]"
@@ -11,20 +10,13 @@ fi
 
 shift
 shift
-shift
 
 if [[ -z ${tag} ]]; then
     tag="latest"
-fi
-
-if [[ ${multiarch} == "multiarch" ]]; then
-    platform="linux/amd64,linux/arm/v7"
-else
-    platform="linux/amd64"
 fi
 
 script_dir=$(dirname $(realpath $0))
 dir="${script_dir}/${image}"
 full_tag="registry.gitlab.com/programie/dockerimages/${image}:${tag}"
 
-docker build build --pull --push --no-cache "$@" --tag "${full_tag}" --platform "${platform}" "${dir}"
+docker build build --pull --push --no-cache "$@" --tag "${full_tag}" "${dir}"
